1d4faa9ebcbd64b72a47272635d0feb6391bdefa
Theophile GREGOIRE <theophile.gregoire@icloud.com>
2021-05-28 17:26:56 +0200

    Setup Maven

---
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -0,0 +1,43 @@
+package fr.lernejo;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class SampleTest {
+    static final Sample s = new Sample();
+
+    @Test
+    void Add_simple() {
+        assertEquals(2, s.op(Sample.Operation.ADD, 1,1));
+    }
+
+    @Test
+    void Add_negative() {
+        assertEquals(-10, s.op(Sample.Operation.ADD, 10,-20));
+    }
+
+    @Test
+    void Mult_simple() {
+        assertEquals(2, s.op(Sample.Operation.MULT, 1,2));
+    }
+
+    @Test
+    void Mult_simple2() {
+        assertEquals(100, s.op(Sample.Operation.MULT, 10,10));
+    }
+
+    @Test
+    void Fact_zero() {
+        assertEquals(1, new Sample().fact(0));
+    }
+
+    @Test
+    void Fact_ten() {
+        assertEquals(3628800, new Sample().fact(10));
+    }
+
+    @Test
+    void Fact_negative() {
+        assertThrows(IllegalArgumentException.class, () -> new Sample().fact(-10));
+    }
+}
